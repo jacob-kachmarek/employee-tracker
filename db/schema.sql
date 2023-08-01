2330aceff978d1974b9f13a3aeb38383d04bc4f4
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-08-01 15:56:52 -0700

    updating job_title table with new table name updating employee table with a foreign key/reffernce for the job title table

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,10 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    dep_name VARCHAR(30)
+    dep_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE job_title (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
@@ -21,7 +21,9 @@ CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    job_title_id INT NOT NULL,
+    FOREIGN KEY (job_title_id)
+    REFERENCES job_title(id),
     manager_id INT NOT NULL,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (dep_name)
+VALUES 
+("Sales"),
+("Accounting"),
+("Data Analysis");
+
+INSERT INTO job_title (title, salary, dep_id)
+VALUES
+("Accountant", 200000, 2),
+("Accounting lead", 300000, 2),
+("Sales Rep", 200000, 1),
+("Sales Lead", 300000, 1),
+("Analyst", 200000, 3),
+("Analyst Lead", 300000, 3);
+
+INSERT INTO employee ()
+VALUES
+(),
+(),
+(),
+(),
+(),
+();
