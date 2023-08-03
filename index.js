d8fe474bd908601da046c6961ba20b4dc076cad9
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-08-03 15:27:55 -0700

    adding questions and options for the dropdown of options user can select to view add and update tables

--- index.js
+++ index.js
@@ -10,3 +10,41 @@ const db = mysql.createConnection(
         database: 'tracker_db'
     }
 );
+
+db.connect(() => options());
+
+const questions = [{
+    type: "list",
+    name: "options",
+    message: "What do you want to do?",
+    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+}];
+
+
+const options = () => {
+    inquirer.prompt(questions).then(res => {
+        switch (res.options) {
+            case "View all departments":
+                viewAllDepartments()
+                break;
+            case "View all roles":
+                viewAllRoles()
+                break;
+            case "View all employees":
+                viewAllEmployees()
+                break;
+            case "Add a department":
+                addNewDepartment()
+                break;
+            case "Add a role":
+                addNewRole()
+                break;
+            case "Add an employee":
+                addNewEmployee()
+                break;
+            case "Update an employee role":
+                updateEmployeeRole()
+                break;
+        }
+    })
+};
